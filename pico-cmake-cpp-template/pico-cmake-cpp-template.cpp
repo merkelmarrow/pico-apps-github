5ff4dacafcbc8a055b810aba9f548300bbbd749e
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-01-29 20:48:53 +0000

    cleanup: remove visual studio boilerplate code

--- pico-cmake-cpp-template/pico-cmake-cpp-template.cpp
+++ pico-cmake-cpp-template/pico-cmake-cpp-template.cpp
@@ -1,12 +1,8 @@
-﻿// pico-cmake-cpp-template.cpp : Defines the entry point for the application.
-//
-
-#include "pico-cmake-cpp-template.h"
+﻿// pico-cmake-cpp-template.cpp
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
 	return 0;
 }

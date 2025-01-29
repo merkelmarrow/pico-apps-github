cae038e836356c22356623e05e209e7f6cb8a185
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-01-29 18:57:21 +0000

    chore(lab01): copy initial skeleton code for lab 01 setup

--- labs/lab01/lab01.c
+++ labs/lab01/lab01.c
@@ -1,11 +1,36 @@
+#include "pico/stdlib.h"
+
 /**
- * @brief LAB #01 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
+ * @brief EXAMPLE - BLINK_C
+ *        Simple example to initialise the built-in LED on
+ *        the Raspberry Pi Pico and then flash it forever.
+ *
+ * @return int  Application return code (zero for success).
  */
 int main() {
 
-    // Returning zero indicates everything went okay.
+    // Specify the PIN number and sleep delay
+    const uint LED_PIN = 25;
+    const uint LED_DELAY = 500;
+
+    // Setup the LED pin as an output.
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Do forever...
+    while (true) {
+
+        // Toggle the LED on and then sleep for delay period
+        gpio_put(LED_PIN, 1);
+        sleep_ms(LED_DELAY);
+
+        // Toggle the LED off and then sleep for delay period
+        gpio_put(LED_PIN, 0);
+        sleep_ms(LED_DELAY);
+
+    }
+
+    // Should never get here due to infinite while-loop.
     return 0;
+
 }

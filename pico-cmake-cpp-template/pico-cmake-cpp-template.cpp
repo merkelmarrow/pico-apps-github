b88ad78fba39fae1ce25451dbbd5c1e37c2604b5
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-01-29 20:44:43 +0000

    init: initialise visual studio template project

---
+++ pico-cmake-cpp-template/pico-cmake-cpp-template.cpp
@@ -0,0 +1,12 @@
+﻿// pico-cmake-cpp-template.cpp : Defines the entry point for the application.
+//
+
+#include "pico-cmake-cpp-template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

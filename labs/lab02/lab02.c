cf917901183cec2bc317214ffdb69d971b384ce6
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-02-10 13:55:51 +0000

    init: initialise lab02 and implement wallis prod funcs

--- labs/lab02/lab02.c
+++ labs/lab02/lab02.c
@@ -1,19 +1,52 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "pico/stdlib.h"
 #include "pico/float.h"     // Required for using single-precision variables.
 #include "pico/double.h"    // Required for using double-precision variables.
 
+//#define WOKWI             // Uncomment if running on Wokwi RP2040 emulator.
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+float wallis_prod_float(int n);
+double wallis_prod_double(int n);
 
 /**
- * @brief LAB #02 - TEMPLATE
- *        Main entry point for the code.
- * 
- * @return int      Returns exit-status zero on completion.
+ * @brief EXAMPLE - HELLO_C
+ *        Simple example to initialise the IOs and then
+ *        print a "Hello World!" message to the console.
+ *
+ * @return int  Application return code (zero for success).
  */
 int main() {
 
+#ifndef WOKWI
+    // Initialise the IO as we will be using the UART
+    // Only required for hardware and not needed for Wokwi
+    stdio_init_all();
+#endif
+
+    // Print a console message to inform user what's going on.
+    printf("Hello World!\n");
+
     // Returning zero indicates everything went okay.
     return 0;
+}
+
+float wallis_prod_float (int n) {
+    float product = 4 / 3;
+    for (int i = 2; i <= n; i++) {
+        float multiplier = ((2 * i) / (2 * i - 1)) * ((2 * i) / (2 * i + 1));
+        product *= multiplier;
+    }
+    return product;
+}
+
+double wallis_prod_double (int n) {
+    double product = 4 / 3;
+    for (int i = 2; i <= n; i++) {
+        double multiplier = ((2 * i) / (2 * i - 1)) * ((2 * i) / (2 * i + 1));
+        product *= multiplier;
+    }
+    return product;
 }

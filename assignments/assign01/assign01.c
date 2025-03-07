5330e8f7913d109bf1442fd263f092f592e06a2f
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-07 12:26:45 +0000

    init: set up initial C wrapper functions

--- assignments/assign01/assign01.c
+++ assignments/assign01/assign01.c
@@ -1,10 +1,27 @@
-// Must declare the main assembly entry point before use.
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
 void main_asm();
 
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
+void asm_gpio_init(uint pin) {
+    gpio_init(pin);
+}
+
+void asm_gpio_set_dir(uint pin, bool out) {
+    gpio_set_dir(pin, out);
+}
+
+bool asm_gpio_get(uint pin) {
+    return gpio_get(pin);
+}
+
+void asm_gpio_put(uint pin) {
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
+}
+
 int main() {
+    stdio_init_all();
     main_asm();
-    return(0);
+    return 0;
 }
